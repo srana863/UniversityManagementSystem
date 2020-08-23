7abef579fafc5f0164c99f2e46e7bd1ab1e0a2fc
Md Sohel Rana <sohel_rana_raju@yahoo.com>
2020-08-23 17:23:55 +0600

    User info service class

--- UniversityManagementSystem/Service/Security/UserInfoManager.cs
+++ UniversityManagementSystem/Service/Security/UserInfoManager.cs
@@ -6,11 +6,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Model.Security;
 using Utilities.Common;
+using System.Transactions;
+using Repository.Common;
+using Repository.Security;
 
 namespace Service.Security
 {
     public class UserInfoManager : BllCommon, IUserInfoManager
     {
+        private readonly IUserInfoRepository _userInfoRepository;
+        public UserInfoManager()
+        {
+            _userInfoRepository = new UserInfoRepository(_dbContext);
+        }
         public ReturnMessage Delete(int id, int companyId)
         {
             throw new NotImplementedException();
@@ -23,12 +31,44 @@ namespace Service.Security
 
         public IEnumerable<UserInfo> GetAll(int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dbContext.Open();
+                return null;
+            }
+            catch (Exception e)
+            {
+                _dbContext.Close();
+                return null;
+            }
+            finally
+            {
+                _dbContext.Close();
+                _dbContext.Dispose();
+            }
         }
 
         public ReturnMessage Save(UserInfo entity, AppSession appSession)
         {
-            throw new NotImplementedException();
+            using (var transactionScope = new TransactionScope(TransactionScopeOption.RequiresNew))
+            {
+                try
+                {
+                    _dbContext.Open();
+                    var isSaved = 0;
+                    return _vmReturn;
+                }
+                catch (Exception e)
+                {
+                    _dbContext.Close();
+                    return _vmReturn;
+                }
+                finally
+                {
+                    _dbContext.Close();
+                    _dbContext.Dispose();
+                }
+            }
         }
     }
 }
